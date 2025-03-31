905cbb951cabd362b3c59258d747f2dbc01a1c87
minseok <minsuk6803@gmail.com>
2025-03-31 20:54:34 +0900

    first commit

---
+++ src/main/java/Midas/Saessack/SaessackApplication.java
@@ -0,0 +1,13 @@
+package Midas.Saessack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaessackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaessackApplication.class, args);
+	}
+
+}
